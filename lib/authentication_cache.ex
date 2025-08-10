75b3cb9a81ad12dccd82a6555428a1ece8b38103
Taylor Howell <66977633+EarliestFall988@users.noreply.github.com>
2025-08-10 18:14:28 -0500

    adding docs to the auth cache gen server, adding a remove token function

--- lib/authentication_cache.ex
+++ lib/authentication_cache.ex
@@ -13,16 +13,38 @@ defmodule Clerk.AuthenticationCache do
     {:ok, %{}}
   end
 
+  @doc """
+
+  Fetch the table name for more interaction with the clerk authentication cache
+  """
   def get_table_name, do: @table_name
 
+  @doc """
+
+  Lookup a token to see if has been cached
+
+  """
   def lookup_token(token) do
     IO.inspect({:lookup, token})
     :ets.lookup(@table_name, token)
   end
 
+  @doc """
+
+  Insert a new session into the cache
+  """
   def insert(token, session, user) do
     time = System.os_time(:seconds)
     IO.inspect({:inserting_data, session, token})
     :ets.insert(@table_name, {token, {session, user, time}})
   end
+
+  @doc """
+
+  Removes an existing token in the session cache.
+
+  This is helpful for scenarios when someone is banned, or when they log out - here you can activate a webhook and trigger a logout
+  """
+  def remove(token), do: :ets.delete(@table_name, token)
+
 end
